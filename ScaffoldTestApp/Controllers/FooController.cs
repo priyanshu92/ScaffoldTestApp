cd55ba441f6ae1f768971b516371dca252368b52
Priyanshu Agrawal <priyanshuagrawal@outlook.com>
2018-12-31 21:21:26 +0530

    Initial Commit

---
+++ ScaffoldTestApp/Controllers/FooController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScaffoldTestApp.Models;
+using System.Collections.Generic;
+
+namespace ScaffoldTestApp.Controllers
+{
+    public class FooController : Controller
+    {
+        private static List<Foo> items = new List<Foo>();
+
+        public ActionResult Index()
+        {
+            return View(items);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Foo foo)
+        {
+            try
+            {
+                items.Add(foo);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ScaffoldTestApp/Models/Foo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ScaffoldTestApp.Models
+{
+    public class Foo
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<Bar> Bars { get; set; } = new List<Bar>();
+    }
+
+    public class Bar
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
